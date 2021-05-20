6beda5effb8a2e9034e71c059acf315df24b6a03
mkrkelj1 <43376568+mkrkelj1@users.noreply.github.com>
2021-05-20 18:13:43 -0400

    Updated range input onChange logic.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import useSwr from 'swr'
-import Link from 'next/link'
-import React, { useState } from 'react';
+import useSwr from "swr";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const fetcher = (url) => fetch(url).then((res) => res.json())
-
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 // export default function Index() {
 //   const { data, error } = useSwr('/api/users', fetcher)
@@ -26,92 +25,167 @@ const fetcher = (url) => fetch(url).then((res) => res.json())
 //   )
 // }
 
+// http://jsfiddle.net/EL4tf/
+
+// {Object.keys(sliderValues).map((slider) => (
+//   <div>
+//     <label>{sliderValues[slider]}</label>
+
+//     <input
+//       name={slider}
+//       type="range"
+//       min={1}
+//       max={100}
+//       value={sliderValues[slider]}
+//       onChange={checkIfMaxReached}
+//       required
+//     />
+//   </div>
+// ))}
+
+// const [sliderValues, setSliderValues] = useState({
+//   slider1: 30,
+//   slider2: 60,
+// });
+
+const maxTotal = 100;
+
 const budgetForm = () => {
-  const [slider1, setSlider1] = useState(30);
-  const [slider2, setSlider2] = useState(60);
-  const [maxReached, setMaxReached] = useState(false)
-  const [currentSlider, setCurrentSlider] = useState(null)
-  var dict = {
-  "slider1":setSlider1,
-  "slider2": setSlider2
-  };
 
-  const maxTotal = 100;
-  var currentTotal = slider1 + slider2
-  function handleSlide(e){
-    const name = e.target.name
-    setCurrentSlider(name)
-    currentTotal = slider1 + slider2
-    if(currentTotal > maxTotal){
-      setMaxReached(true)
-      console.log(maxReached);
-      console.log(e.target.value);
-      console.log(currentTotal)
-      dict[name](parseFloat(e.target.value ) - (currentTotal - maxTotal))
+  const [sliderValues, setSliderValues] = useState({
+    slider1: 5,
+    slider2: 10,
+    slider3: 0,
+  });
 
-      e.preventDefault()
-    }
-    else{
-      setMaxReached(false)
-      console.log(maxReached)
-      console.log(currentTotal)
-      dict[name](parseFloat(e.target.value))
+  const getTotal = () => Object.values(sliderValues).reduce((a, b) => a + b, 0);
 
+  function handleChange(event) {
+    const name = event.target.name; // Name of this slider.
+    const oldVal = sliderValues[name] // Current value in state.
+    const newVal = parseInt(event.target.value); // Value of change event.
+    
+    /*
+    * Get the current total from state, 
+    * minus the "old"/CURRENT value and
+    * plus the "new"/THIS value
+    * (we're just updating the old value with the new one).
+    */
+    const total = getTotal() - oldVal + newVal;
 
-    }
-  }
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-    // Get form data
-    let cat_1 = e.currentTarget.slider1.value;
-    let cat_2 = e.currentTarget.slider2.value;
-    console.log(cat_1, cat_2);
-    try {
-      fetch("/api/form", {
-        method: "POST",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(cat_1)
-      })
+    // If total is less than 100, update state. 
+    if (total <= 100) {
+      const updatedState = {
+        ...sliderValues,
+        [name]: newVal,
+      };
+      setSliderValues(updatedState);
     } 
-    catch (error){
-      console.log(error)
-    }
+
+    // If not, don't do anything.
+    return null
   }
+
   return (
     <div>
-      <h1>Fake form</h1>
-      <form onSubmit={handleSubmit}>
+      <h2>Fake form</h2>
+
+      <div style={{ paddingBottom: "20px" }}>
         <div>
-          <div>
-         {maxReached ? 
-            <input name="slider1" type="range" min={1} max={100} defaultValue = {slider1} 
-            value = {slider1} onChange = {handleSlide} required />
-              :
-              <input name="slider1" type="range" min={1} max={100} defaultValue = {slider1}
-              onChange = {handleSlide} required />
-            }
-          </div>
-          <div>
-           {maxReached ? 
-            <input name="slider2" type="range" min={1} max={100} defaultValue = {slider2} 
-            value = {slider2} onChange = {handleSlide} required />
-              :
-              <input name="slider2" type="range" min={1} max={100} defaultValue = {slider2}
-              onChange = {handleSlide} required />
-            }
-           {/* <input name="slider2" type="range" min={1} max={100} defaultValue = {slider2} 
-               required />*/}
-          </div>
-          <div>
-            <input name="cat_3" type="number" defaultValue = "30" required />
-          </div>
+          <label>{sliderValues.slider1}</label>
         </div>
+
+        <input
+          name="slider1"
+          type="range"
+          min={0}
+          max={100}
+          value={sliderValues.slider1}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div>
         <div>
-          <button type="submit">Submit</button>
+          <label>{sliderValues.slider2}</label>
         </div>
-      </form>
+        <input
+          name="slider2"
+          type="range"
+          min={0}
+          max={100}
+          value={sliderValues.slider2}
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div style={{ paddingTop: "20px"}}>
+        <div>
+          <label>{sliderValues.slider3}</label>
+        </div>
+
+        <input
+          name="slider3"
+          type="range"
+          min={0}
+          max={100}
+          step="1"
+          value={sliderValues.slider3}
+          onChange={handleChange}
+        />
+      </div>
     </div>
   );
 };
 export default budgetForm;
+
+
+// async function handleSubmit(e) {
+//   e.preventDefault();
+//   // Get form data
+//   let cat_1 = e.currentTarget.slider1.value;
+//   let cat_2 = e.currentTarget.slider2.value;
+//   console.log(cat_1, cat_2);
+//   try {
+//     fetch("/api/form", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify(cat_1),
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// <form onSubmit={handleSubmit}>
+//   <div>
+//     <div>
+//    {maxReached ?
+//       <input name="slider1" type="range" min={1} max={100} defaultValue = {slider1}
+//       value = {slider1} onChange = {handleSlide} required />
+//         :
+//         <input name="slider1" type="range" min={1} max={100} defaultValue = {slider1}
+//         onChange = {handleSlide} required />
+//       }
+//     </div>
+//     <div>
+//      {maxReached ?
+//       <input name="slider2" type="range" min={1} max={100} defaultValue = {slider2}
+//       value = {slider2} onChange = {handleSlide} required />
+//         :
+//         <input name="slider2" type="range" min={1} max={100} defaultValue = {slider2}
+//         onChange = {handleSlide} required />
+//       }
+//      {/* <input name="slider2" type="range" min={1} max={100} defaultValue = {slider2}
+//          required />*/}
+//     </div>
+//     <div>
+//       <input name="cat_3" type="number" defaultValue = "30" required />
+//     </div>
+//   </div>
+//   <div>
+//     <button type="submit">Submit</button>
+//   </div>
+// </form>
