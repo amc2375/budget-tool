db9c97b0463bd9c3f1da03911f5b921d91b21fc1
Miles McLeod <milesmcleod123@gmail.com>
2021-08-02 12:44:00 -0700

    finish About styles

--- components/home/content/AboutUs.js
+++ components/home/content/AboutUs.js
@@ -6,12 +6,18 @@ import styles from './AboutUs.module.scss';
 export default function AboutUs() {
 
   return (
-    <React.Fragment>
-      <p>{"Who we are"}</p>
-      <Image src={bcdiLogoColor} alt={"Bronx Cooperative Development Initiative logo"}/>
-      <p>{"The Bronx Cooperative Development Initiative (BCDI) is a community-led effort to build an equitable, sustainable, and democratic local economy that creates wealth and ownership for low-income people of color—what we call economic democracy."}</p>
-      <BarnardUrbanStudiesLogo/>
-      <p>{"The Barnard–Columbia Urban Studies program enables students to explore and understand the urban experience in all of its richness and complexity.  It recognizes the city as an amalgam of diverse peoples and their social, political, economic, and cultural interactions within a distinctive built environment."}</p>
-    </React.Fragment>
+    <article className={styles.container}>
+      <div className={styles.left}>
+        <h1>{"Who we are"}</h1>
+      </div>
+      <div className={styles.middle}>
+        <Image src={bcdiLogoColor} alt={"Bronx Cooperative Development Initiative logo"}/>
+        <p>{"The Bronx Cooperative Development Initiative (BCDI) is a community-led effort to build an equitable, sustainable, and democratic local economy that creates wealth and ownership for low-income people of color—what we call economic democracy."}</p>
+      </div>
+      <div className={styles.right}>
+        <BarnardUrbanStudiesLogo/>
+        <p>{"The Barnard–Columbia Urban Studies program enables students to explore and understand the urban experience in all of its richness and complexity.  It recognizes the city as an amalgam of diverse peoples and their social, political, economic, and cultural interactions within a distinctive built environment."}</p>
+      </div>
+    </article>
   );
 };
