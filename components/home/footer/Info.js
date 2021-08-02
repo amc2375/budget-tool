346a08c1c2db9c37aebbfa4ce6f632309a16c771
Miles McLeod <milesmcleod123@gmail.com>
2021-08-02 14:16:45 -0700

    make footer links work

--- components/home/footer/Info.js
+++ components/home/footer/Info.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import Link from 'next/link';
 import styles from './Info.module.scss';
 export default function Info() {
 
   return (
     <React.Fragment>
       <div className={styles.container}>
-        <p>{"Home"}</p>
-        <p>{"Participatory Budgeting Survey"}</p>
+        <Link href={"/"}>
+          <a>{"Home"}</a>
+        </Link>
+        <Link href={"/budget-survey"}>
+          <a>{"Participatory Budgeting Survey"}</a>
+        </Link>
       </div>
       <div className={styles.container}>
         <p>{"Bronx Cooperative Development Initiative"}</p>
