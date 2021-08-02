a8e95188948da47b0779c95494ab3d536bd4cd2c
Miles McLeod <milesmcleod123@gmail.com>
2021-08-02 12:25:17 -0700

    top half of about content on home page styled

--- components/home/content/AboutThis.js
+++ components/home/content/AboutThis.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import Image from 'next/image';
+import bronxDots from '../../../static/bronx_dots.png';
 import styles from './AboutThis.module.scss';
 export default function AboutThis() {
 
   return (
-    <React.Fragment>
-      <p>{"What is this?"}</p>
-      <p>{"The Bronx already has much of what it needs to transform its local economy and become a thriving, healthy borough. However, Bronxites currently do not have enough meaningful ownership or control over our futures. To ensure existing residents who fought for the Bronx can continue to call it home for generations to come, a coalition of Bronx organizations is engaging residents and local stakeholders in the creation of a Bronx-wide Platform and Plan to establish a community-driven, comprehensive vision for economic development rooted in advancing racial justice and economic democracy."}</p>
-      <p>{"In order to address root causes, combat generational systems failures, and meaningfully respond to the urgent pressures that are threatening the Bronx’s status as the last affordable borough for low-income and working-class people in New York City, the Bronx requires a vision and plan for equitable economic development that is ambitious, coordinated, and long-term."}</p>
-    </React.Fragment>
+    <article className={styles.container}>
+      <div className={styles.left}>
+        <h1>{"What is this?"}</h1>
+        <p>{"The Bronx already has much of what it needs to transform its local economy and become a thriving, healthy borough. However, Bronxites currently do not have enough meaningful ownership or control over our futures. To ensure existing residents who fought for the Bronx can continue to call it home for generations to come, a coalition of Bronx organizations is engaging residents and local stakeholders in the creation of a Bronx-wide Platform and Plan to establish a community-driven, comprehensive vision for economic development rooted in advancing racial justice and economic democracy."}</p>
+        <p>{"In order to address root causes, combat generational systems failures, and meaningfully respond to the urgent pressures that are threatening the Bronx’s status as the last affordable borough for low-income and working-class people in New York City, the Bronx requires a vision and plan for equitable economic development that is ambitious, coordinated, and long-term."}</p>
+      </div>
+      <div className={styles.right}>
+        <Image src={bronxDots} alt={"Image of the Bronx Borough Footprint"}/>
+      </div>
+    </article>
   );
 };
--- components/home/content/Content.js
+++ components/home/content/Content.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
 import AboutThis from './AboutThis.js';
 import AboutUs from './AboutUs.js';
-import bronxDots from '../../../static/bronx_dots.png';
 import styles from './Content.module.scss';
 export default function Content() {
 
@@ -10,7 +8,6 @@ export default function Content() {
     <section className={styles.container}>
       <AboutThis/>
       <AboutUs/>
-      <Image src={bronxDots} alt={"Image of the Bronx Borough Footprint"}/>
     </section>
   );
 };
