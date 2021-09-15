5378276df8d22d5f9f4f316c2579777dc830d5bb
Miles McLeod <milesmcleod123@gmail.com>
2021-09-15 13:29:02 -0700

    Add media query library in react

--- components/nav/Menu.js
+++ components/nav/Menu.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavLink from './NavLink.js';
 import styles from './Menu.module.scss';
+import { useMediaQuery } from 'react-responsive';
+import Hamburger from '../../static/hamburger.svg';
+import X from '../../static/x.svg';
 export default function Menu() {
 
-  return (
-    <section className={styles.container}>
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(
+    false
+  );
+
+  const isMobile = useMediaQuery({ query: '(max-width: 960px)' });
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const links = (
+    <React.Fragment>
       <NavLink text={"Home"} path={"/"}/>
       <NavLink text={"Take the Survey"} path={"/budget-survey"}/>
       <NavLink text={"About the Project"} path={"/#about-this"}/>
       <NavLink text={"Who We Are"} path={"/#about-us"}/>
-    </section>
+    </React.Fragment>
+  );
+
+  return (
+    <React.Fragment>
+      <div
+        className={styles.hamburger}
+        onClick={toggleMobileMenu}>
+        {
+          !mobileMenuOpen ? <Hamburger/> : <X/>
+        }
+        <Hamburger/>
+      </div>
+      <section className={styles.container}>
+        {links}
+      </section>
+    </React.Fragment>
   );
 };
