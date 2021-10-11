c22c38e34624ce2b441179afe5390b6de7ff170e
Miles McLeod <milesmcleod123@gmail.com>
2021-10-11 15:30:08 -0700

    complete styles for the about page for desktop

--- components/aboutTheProject/UrbanStudiesCollab.js
+++ components/aboutTheProject/UrbanStudiesCollab.js
@@ -12,6 +12,7 @@ export default function UrbanStudiesCollab() {
         <h3>{"Barnard-Columbia Urban Studies Program"}</h3>
         <div className={styles.columns}>
           <p>{"The Barnard + Columbia Urban Studies program enables students to explore and understand the urban experience in all of its richness and complexity. It recognizes the city as an amalgam of diverse peoples and their social, political, economic, and cultural interactions within a distinctive built environment."}</p>
+          <div className={styles.spacer}/>
           <p>{"Funding for this project was provided by a Barnard Engages New York: Collaborative Partnerships to Promote Sustainable Change (or beNY for short) award."}</p>
         </div>
       </div>
--- components/aboutTheProject/ValuesAndPrinciples.js
+++ components/aboutTheProject/ValuesAndPrinciples.js
@@ -5,14 +5,14 @@ export default function ValuesAndPrinciples() {
   return (
     <section className={styles.container}>
       <h1>{"The Bronx-wide Coalition’s Values and Principles"}</h1>
-      <ol>
+      <ul>
         <li>{"We address injustice on a systemic level."}</li>
         <li>{"We center racial justice."}</li>
         <li>{"We apply an intergenerational lens."}</li>
         <li>{"We apply a gender justice lens."}</li>
         <li>{"We apply an environmental sustainability lens."}</li>
         <li>{"We use economic democracy as a framework from which to address poverty and inequality."}</li>
-      </ol>
+      </ul>
     </section>
   );
 };
