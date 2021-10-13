6bdec1238fa8006f5c2f704c48219c2af6176107
Miles McLeod <milesmcleod123@gmail.com>
2021-10-13 01:25:22 -0700

    complete working version of the modal on desktop and mobile

--- components/form/header/Inputs.js
+++ components/form/header/Inputs.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import React from 'react';
 import CouncilDistrictInput from './CouncilDistrictInput.js';
 import ZipCodeInput from './ZipCodeInput.js';
 import BudgetFamiliarityInput from './BudgetFamiliarityInput.js';
@@ -12,46 +13,50 @@ export default function Inputs({
     setZipCode,
     budgetFamiliarity,
     setBudgetFamiliarity,
-    modalBackgroundClass,
-    modalBodyClass
+    continueButtonIsDisabled,
+    setContinueButtonIsDisabled,
+    inputsAreHidden,
+    setInputsAreHidden
   }) {
 
-  const [isDisabled, setIsDisabled ] = useState(
-    false
-  );
-
-  useEffect(() => {
-    if (zipCode.length === 5 && budgetFamiliarity && district) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true);
-    }
-  }, [zipCode, district, budgetFamiliarity])
-
-  const handleContinue = (e) => {
-    const background = document.getElementsByClassName(`${modalBackgroundClass}`);
-    const modal = document.getElementsByClassName(`${modalBodyClass}`);
-    if (background[0]) background[0].remove();
-    if (modal[0]) modal[0].remove();
+  const handleIntermediarySkipMobileToggle = (e) => {
+    setInputsAreHidden(!inputsAreHidden);
+    setContinueButtonIsDisabled(!continueButtonIsDisabled);
+    district === "0" ? setDistrict("") : setDistrict("0");
+    zipCode === "00000" ? setZipCode("") : setZipCode("00000");
+    budgetFamiliarity === "0" ? setBudgetFamiliarity("") : setBudgetFamiliarity("0");
   }
 
   return (
-    <section className={styles.container}>
-      <strong>{"Bronx Residents"}</strong>
-      <BudgetFamiliarityInput
-        budgetFamiliarity={budgetFamiliarity}
-        setBudgetFamiliarity={setBudgetFamiliarity}/>
-      <CouncilDistrictInput
-        districts={districts}
-        setDistrict={setDistrict}/>
-      <ZipCodeInput
-        zipCode={zipCode}
-        setZipCode={setZipCode}/>
-      <button
-        type={"button"}
-        onClick={handleContinue}
-        disabled={isDisabled}
-        className={isDisabled ? styles.buttonDisabled : styles.buttonEnabled}>{"Continue"}</button>
-    </section>
+    <React.Fragment>
+      {
+        !inputsAreHidden ? (
+          <div
+            onClick={handleIntermediarySkipMobileToggle}
+            className={styles.skipMobile}>
+            <p>Live outside the bronx?</p>
+            <p>&#8594;</p>
+          </div>
+        ) : (
+          <div
+            onClick={handleIntermediarySkipMobileToggle}
+            className={styles.skipMobile}>
+            <p>&#8592; &nbsp;&nbsp;&nbsp;Back</p>
+          </div>
+        )
+      }
+      <section className={inputsAreHidden ? styles.hide : styles.container}>
+        <strong>{"Bronx Residents"}</strong>
+        <BudgetFamiliarityInput
+          budgetFamiliarity={budgetFamiliarity}
+          setBudgetFamiliarity={setBudgetFamiliarity}/>
+        <CouncilDistrictInput
+          districts={districts}
+          setDistrict={setDistrict}/>
+        <ZipCodeInput
+          zipCode={zipCode}
+          setZipCode={setZipCode}/>
+      </section>
+    </React.Fragment>
   );
 };
--- components/form/modal/FormModal.js
+++ components/form/modal/FormModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Inputs from '../header/Inputs.js';
 import styles from './FormModal.module.scss';
 
@@ -12,7 +12,33 @@ export default function FormModal({
     setBudgetFamiliarity
   }) {
 
+  const [continueButtonIsDisabled, setContinueButtonIsDisabled ] = useState(
+    false
+  );
+
+  const [inputsAreHidden, setInputsAreHidden ] = useState(
+    false
+  );
+
+  useEffect(() => {
+    if (zipCode.length === 5 && budgetFamiliarity && district) {
+      setContinueButtonIsDisabled(false);
+    } else {
+      setContinueButtonIsDisabled(true);
+    }
+  }, [zipCode, district, budgetFamiliarity])
+
+  const handleContinue = (e) => {
+    const background = document.getElementsByClassName(`${styles.modalBackground}`);
+    const modal = document.getElementsByClassName(`${styles.modalBody}`);
+    if (background[0]) background[0].remove();
+    if (modal[0]) modal[0].remove();
+  }
+
   const handleSkip = (e) => {
+    setDistrict("0");
+    setZipCode("00000");
+    setBudgetFamiliarity("0");
     const background = document.getElementsByClassName(`${styles.modalBackground}`);
     const modal = document.getElementsByClassName(`${styles.modalBody}`);
     if (background[0]) background[0].remove();
@@ -29,19 +55,32 @@ export default function FormModal({
         <section className={styles.introduction}>
           <p>{"We believe the people of the Bronx and New York City should have a more direct say in how the budget is created, so that it reflects the true needs and priorities of its residents."}</p>
         </section>
-        <section className={styles.selection}>
-          <Inputs
-            zipCode={zipCode}
-            districts={districts}
-            district={district}
-            setDistrict={setDistrict}
-            setZipCode={setZipCode}
-            budgetFamiliarity={budgetFamiliarity}
-            setBudgetFamiliarity={setBudgetFamiliarity}
-            modalBackgroundClass={`${styles.modalBackground}`}
-            modalBodyClass={`${styles.modalBody}`}
-            />
-          <div className={styles.skip}>
+        <section className={styles.selectionWrapper}>
+          <div className={styles.selection}>
+            <Inputs
+              zipCode={zipCode}
+              districts={districts}
+              district={district}
+              setDistrict={setDistrict}
+              setZipCode={setZipCode}
+              budgetFamiliarity={budgetFamiliarity}
+              setBudgetFamiliarity={setBudgetFamiliarity}
+              continueButtonIsDisabled={continueButtonIsDisabled}
+              setContinueButtonIsDisabled={setContinueButtonIsDisabled}
+              inputsAreHidden={inputsAreHidden}
+              setInputsAreHidden={setInputsAreHidden}
+              />
+            <div className={!inputsAreHidden ? styles.hide : styles.skipMobile}>
+              <strong>{"I live outside of the Bronx"}</strong>
+              <p>{"We appreciate your interest in this project, feel free to try out the tool!"}</p>
+            </div>
+            <button
+              type={"button"}
+              onClick={handleContinue}
+              disabled={continueButtonIsDisabled}
+              className={continueButtonIsDisabled ? styles.buttonDisabled : styles.buttonEnabled}>{"Continue"}</button>
+          </div>
+          <div className={styles.skipDesktop}>
             <strong>{"I live outside of the Bronx"}</strong>
             <p>{"We appreciate your interest in this project, feel free to try out the tool!"}</p>
             <button
