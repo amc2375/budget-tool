28b3f4cbdd22f25e3c0677abe902f55b73f74f44
Miles McLeod <milesmcleod123@gmail.com>
2021-10-20 10:46:33 -0700

    continue transforming .sql schema + seeded data

--- utilities/schema.sql
+++ utilities/schema.sql
@@ -23,24 +23,16 @@ INSERT INTO bronx.districts (district_id, name) VALUES (16, 'Gibson');
 INSERT INTO bronx.districts (district_id, name) VALUES (17, 'Salamanca Jr.');
 INSERT INTO bronx.districts (district_id, name) VALUES (18, 'Ruben Diaz Sr.');
 
-/* create new table of contexts with amount and description */
-DROP TABLE IF EXISTS bronx.category_contexts;
-CREATE SEQUENCE IF NOT EXISTS category_context_sequence_id;
-CREATE TABLE bronx.category_contexts (
-	"id" int4 NOT NULL DEFAULT nextval('category_context_sequence_id'::regclass),
+/* create the table of categories */
+DROP TABLE IF EXISTS bronx.categories;
+CREATE SEQUENCE IF NOT EXISTS category_sequence_id;
+CREATE TABLE bronx.categories (
+	"id" int4 NOT NULL DEFAULT nextval('category_sequence_id'::regclass),
+	"name" text NOT NULL,
+	"descriptive_html" text NOT NULL,
 	"amount" numeric NOT NULL,
-	"description" text NOT NULL,
 	"created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id")
-)
-
-/* fill up the contexts table */
-INSERT INTO bronx.category_contexts (
-	name,
-	url
-) VALUES (
-	'Name',
-	'URL'
 );
 
 /* create new table of links to accompany categories */
@@ -50,74 +42,130 @@ CREATE TABLE bronx.category_links (
 	"id" int4 NOT NULL DEFAULT nextval('category_link_sequence_id'::regclass),
 	"name" text NOT NULL,
 	"url" text NOT NULL,
-	"created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY ("id")
-)
-
-/* fill up the links table */
-INSERT INTO bronx.category_links (
-	name,
-	url
-) VALUES (
-	'Name',
-	'URL'
-);
-
-
-/* create the table of categories */
-DROP TABLE IF EXISTS bronx.categories;
-CREATE SEQUENCE IF NOT EXISTS category_sequence_id;
-CREATE TABLE bronx.categories (
-	"id" int4 NOT NULL DEFAULT nextval('category_sequence_id'::regclass),
-	"name" text NOT NULL,
-	"descriptive_html" text NOT NULL,
-	"amount" numeric NOT NULL,
-	"category_context_id" int4 NOT NULL,
-	"category_link_id" int4,
+	"category_id" int NOT NULL,
 	"created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY ("id"),
-	FOREIGN KEY (category_context_id) REFERENCES bronx.category_contexts(id) ON DELETE CASCADE,
-  FOREIGN KEY (category_link_id) REFERENCES bronx.category_links(id) ON DELETE CASCADE
-);
+	FOREIGN KEY (category_id) REFERENCES bronx.categories(id) ON DELETE CASCADE
+)
+
+/* create new table of 'context' rows with an amount and description */
+DROP TABLE IF EXISTS bronx.category_contexts;
+CREATE SEQUENCE IF NOT EXISTS category_context_sequence_id;
+CREATE TABLE bronx.category_contexts (
+	"id" int4 NOT NULL DEFAULT nextval('category_context_sequence_id'::regclass),
+	"amount" numeric NOT NULL,
+	"description" text NOT NULL,
+	"category_id" int NOT NULL,
+	"created_at" timestamp(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY ("id"),
+	FOREIGN KEY (category_id) REFERENCES bronx.categories(id) ON DELETE CASCADE
+)
 
 /* insert each required category using foreign keys to identify related links and contexts */
+
+/*********** education ***********/
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
-	descriptive_html,
-	context_amount_1,
-	context_description_1,
-	context_amount_2,
-	context_description_2
+	descriptive_html
 ) VALUES (
+	1,
 	'Education',
-	 30000000000.00,
-	 '<p>The <a href="https://www.schools.nyc.gov/" target="_blank"rel="noreferrer noopener">Department of Education (DOE)</a> funds public and charter schools, prekindergarten programs, and afterschool care (sports, music, other educational programming).</p>',
-	 '$10 million',
-	 'One year of school supplies for ~14,000 students',
-	 '$100 million',
-	 '~2.5 months of funding for universal pre-K'
+	32000000000.00,
+	'<p>The Department of Education (DOE) funds public and charter schools, prekindergarten programs, and afterschool care (sports, music, other educational programming).</p>'
 );
 
+/* education context 1 */
+INSERT INTO bronx.category_contexts (
+	amount,
+	description,
+	category_id
+) VALUES (
+	10000000,
+	'One year of school supplies for ~14,000 students',
+	1
+);
+
+/* education context 2 */
+INSERT INTO bronx.category_contexts (
+	amount,
+	description,
+	category_id
+) VALUES (
+	100000000,
+	'~2.5 months of funding for universal pre-K',
+	1
+);
+
+/* education link */
+INSERT INTO bronx.category_links (
+	name,
+	url,
+	category_id
+) VALUES (
+	'Department of Education (DOE)',
+	'https://www.schools.nyc.gov/',
+	1
+);
+
+
+
+
+
+/*********** CUNY ***********/
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
-	descriptive_html,
-	context_amount_1,
-	context_description_1,
-	context_amount_2,
-	context_description_2
+	descriptive_html
 ) VALUES (
+	2,
 	'CUNY',
 	 1000000000.00,
-	 '<p> Education spending also includes funding for the system of 25 colleges across the City that make up <a href="https://www.cuny.edu/" target="_blank" rel="noreferrer noopener">City University of New York</a> (CUNY).</p>',
-	 '$10 million',
-	 'Average financial aid for ~1,150 first-year students at CUNY-Queens College',
-	 '$100 million',
-	 '4-years, free in-state tuition for ~3,607 students'
+	 '<p>In addition to DOE spending, education spending also includes funding for the system of 25 colleges across the City that make up City University of New York (CUNY).</p>'
 );
 
+/* CUNY context 1 */
+INSERT INTO bronx.category_contexts (
+	amount,
+	description,
+	category_id
+) VALUES (
+	10000000,
+	'Average financial aid for ~1,150 first-year students at CUNY-Queens College',
+	2
+);
+
+/* CUNY context 2 */
+INSERT INTO bronx.category_contexts (
+	amount,
+	description,
+	category_id
+) VALUES (
+	100000000,
+	'4-years, free in-state tuition for ~3,607 students',
+	2
+);
+
+/* CUNY link */
+INSERT INTO bronx.category_links (
+	name,
+	url,
+	category_id
+) VALUES (
+	'City University of New York',
+	'https://www.cuny.edu/',
+	2
+);
+
+
+
+
+
+/*********** social services ***********/
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -126,6 +174,7 @@ INSERT INTO bronx.categories (
 	context_amount_2,
 	context_description_2
 ) VALUES (
+	3,
 	'Social Services',
 	 17000000000.00,
 	 '<p>Social Services spending includes funding a number of City agencies that provide human services and assistance. Included in this category are the budgets for <a href="https://www1.nyc.gov/site/veterans/about/about.page" target="_blank" rel="noreferrer noopener">New York City Department of Veteran Services</a>, <a href="https://www1.nyc.gov/site/acs/about/about.page" target="_blank" rel="noreferrer noopener">Administration for Children’s Services</a> (ACS), <a href="https://www1.nyc.gov/site/dss/index.page" target="_blank" rel="noreferrer noopener">NYC Department of Social Services</a> (DSS), <a href="https://www1.nyc.gov/site/dhs/index.page" target="_blank" rel="noreferrer noopener">Department of Homeless Services</a> (DHS), <a href="https://www1.nyc.gov/site/dfta/index.page" target="_blank" rel="noreferrer noopener">Department for the Aging</a>, <a href="https://www.dnb.com/business-directory/company-profiles.new_york_city_financial_information_services_agency.07ef035f503b11f3.html" target="_blank" rel="noreferrer noopener">Financial Information Services Agency</a>, <a href="https://www1.nyc.gov/site/cchr/index.page" target="_blank" rel="noreferrer noopener">NYC Commission on Human Rights</a> and the <a href="https://www1.nyc.gov/site/dycd/index.page" target="_blank" rel="noreferrer noopener">Department on Youth and Community Development</a>. </p>',
@@ -136,6 +185,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -144,6 +194,7 @@ INSERT INTO bronx.categories (
 	context_amount_2,
 	context_description_2
 ) VALUES (
+	4,
 	'Police & Corrections',
 	 7000000000.00,
 	 '<p>The <a href="https://www1.nyc.gov/site/nypd/index.page" target="_blank" rel="noreferrer noopener">Police Department of the City of New York (NYPD)</a> enforces the law, maintains public order, and investigates crimes. Spending in this category also includes funding for the <a href="https://www1.nyc.gov/site/doc/index.page" target="_blank" rel="noreferrer noopener">NYC Department of Corrections</a>. </p>',
@@ -154,6 +205,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -162,6 +214,7 @@ INSERT INTO bronx.categories (
 	context_amount_2,
 	context_description_2
 ) VALUES (
+	5,
 	'General Government',
 	 4000000000.00,
 	 '<p>The General Government consists of the <a href="https://www1.nyc.gov/office-of-the-mayor/"  target="_blank" rel="noreferrer noopener">NYC Office of the Mayor</a> and various administrative departments in the city. It also includes <a href="https://www1.nyc.gov/site/cau/community-boards/community-boards.page" target="_blank" rel="noreferrer noopener">NYC Community Boards</a>, <a href="http://bronxboropres.nyc.gov/" target="_blank" rel="noreferrer noopener">Offices of the Borough Presidents</a>, and <a href="https://council.nyc.gov/" target="_blank" rel="noreferrer noopener">NYC City Council</a>.</p>',
@@ -172,6 +225,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -182,6 +236,7 @@ INSERT INTO bronx.categories (
 	context_amount_3,
 	context_description_3
 ) VALUES (
+	6,
 	'Health and Hospitals',
 	 3000000000.00,
 	 '<p>The New York City <a href="https://www1.nyc.gov/site/doh/index.page" target="_blank" rel="noreferrer noopener">Department of Health and Mental Hygiene (DOH)</a> provides a range of services including health inspections, licensing and no cost health clinics as well as public health research and response. <a href="https://www.nychealthandhospitals.org/" target="_blank" ref="noreferrer noopener">New York Health + Hospital Corps (HHC)</a> is a public healthcare system that includes 11 acute hospital centers, five long term care facilities and numerous clinics providing primary and specialized care. </p>',
@@ -194,6 +249,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -202,6 +258,7 @@ INSERT INTO bronx.categories (
 	context_amount_2,
 	context_description_2
 ) VALUES (
+	7,
 	'Sanitation',
 	 2000000000.00,
 	 '<p>The <a href="https://www1.nyc.gov/assets/dsny/site/home" target="_blank" rel="noreferrer noopener">NYC Department of Sanitation (DSNY)</a> is the world’s largest sanitation department. DSNY collects more than 10,500 tons of residential and institutional garbage and 1,760 tons of the recyclables – each day. While efficiently managing solid waste and clearing litter or snow from 6,300 miles of streets, the Department is also a leader in environmentalism — committing to sending zero waste to landfills.</p>',
@@ -212,6 +269,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -222,6 +280,7 @@ INSERT INTO bronx.categories (
 	context_amount_3,
 	context_description_3
 ) VALUES (
+	8,
 	'Environmental Protection',
 	 1500000000.00,
 	 '<p>The <a href="https://www1.nyc.gov/site/dep/index.page" target="_blank" ref="noreferrer noopener">Department of Environmental Protection (DEP)</a> is responsible for upkeep of water mains and sewers and conducting projects to ensure water quality and conservation as well as control water pollution. DEP  works with multiple City agencies to construct, install, and maintain various green Infrastructure projects for stormwater capture. The department works to supply clean drinking water, collects and treats wastewater, and reduces air, noise, and hazardous substances pollution. </p>',
@@ -234,6 +293,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -242,6 +302,7 @@ INSERT INTO bronx.categories (
 	context_amount_2,
 	context_description_2
 ) VALUES (
+	9,
 	'Housing and Urban Development',
 	1500000000.00,
 	'<p>The <a href="https://www1.nyc.gov/site/hpd/about/about-hpd.page" target="_blank" rel="noreferrer noopener">Department of Housing Preservation and Development (HPD)</a> promotes the quality and affordability of the city''s housing and the strength and diversity of its many neighborhoods. The agency preserves and builds affordable housing and protects tenants. It enforces the building code and engages in neighborhood planning. The <a href="https://www1.nyc.gov/site/planning/about/department.page" target="_blank" rel="noreferrer noopener">Department of City Planning (DCP)</a> is New York City’s primary land use agency and is instrumental in designing the City’s physical and socioeconomic framework. This budget line also includes funding for the New York City <a href="https://www1.nyc.gov/site/buildings/index.page" target="_blank" rel="noreferrer noopener">Department of Buildings (DOB)</a> and the <a href="https://www1.nyc.gov/site/lpc/index.page" target="_blank" rel="noreferrer noopener">Landmarks Preservation Commission</a> that protects New York City''s architecturally, historically, and culturally significant buildings and sites by granting them landmark or historic district status, and regulating them after designation.</p>',
@@ -252,6 +313,7 @@ INSERT INTO bronx.categories (
 );
 
 INSERT INTO bronx.categories (
+	id,
 	name,
 	amount,
 	descriptive_html,
@@ -260,6 +322,7 @@ INSERT INTO bronx.categories (
 	context_amount_2,
 	context_description_2
 ) VALUES (
+	10,
 	'Libraries, Recreation, Culture',
 	 1000000000.00,
 	 '<p>The <a href="https://www1.nyc.gov/site/dcla/index.page" target="_blank" rel="noreferrer noopener">Department of Cultural Affairs</a> (DCLA), and <a href="https://www.nycgovparks.org/" target="_blank" rel="noreferrer noopener">Departments of Parks and Recreation</a>, maintain and promote museum and cultural institutions, public parks, community programs, and other neighborhood- strengthening services. With nearly 53 million items and 92 locations, the <a href="https://www.nypl.org/" target="_blank" rel="noreferrer noopener">New York Public Library</a> is the second largest public library in the United States and the third largest in the world. Libraries allow people to borrow books and other materials, have access to wifi and technology (such as computers and printers). They provide assistance through workshops, public programming and research services.</p>',
@@ -268,7 +331,6 @@ INSERT INTO bronx.categories (
 	 '$10 million',
 	 '952 summer pool and beach season lifeguards (68 percent of seasonal lifeguards)'
 );
-
 
 /* create the budget table to hold user submission data */
 DROP TABLE IF EXISTS bronx.budget;
